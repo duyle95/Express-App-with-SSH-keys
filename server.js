ae540b812c5d171868c33238a090f4d45b427344
DuyLeNguyen <duy.lenguyen@edu.turkuamk.fi>
2018-03-23 16:45:45 +0200

    Setup start script and heroku deploy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 // custom middleware
@@ -46,6 +48,6 @@ app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'bad request' });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
